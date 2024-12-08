88596e82d766f7a58ebec6162334013bb8aa58e6
Marlon "Zeke" Branham <147533734+zekebranham@users.noreply.github.com>
2024-12-08 09:49:14 -0800

    Delete Minesweeper directory

--- Minesweeper/scripts/modes.js
+++
@@ -1,7 +0,0 @@
-const MODES = {
-    beginner: { rows: 8, cols: 8, mines: 10 },
-    intermediate: { rows: 16, cols: 16, mines: 40 },
-    expert: { rows: 16, cols: 30, mines: 99 },
-};
-
-export default MODES;
